7e8d31e44011ffa2dabee7e2e5b1343bdcd894ed
P-Ramos16 <p.ramos@ua.pt>
2025-03-16 18:26:50 +0000

    Altered project structure and cmake

---
+++ src/build/CMakeFiles/MyDLL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyDLL.dir/module/MyDLL.c.o"
+  "CMakeFiles/MyDLL.dir/module/MyDLL.c.o.d"
+  "libMyDLL.a"
+  "libMyDLL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyDLL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
